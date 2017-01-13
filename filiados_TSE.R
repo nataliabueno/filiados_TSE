a7f21567e707ff0cb078799d9273a781c8d86998
Natalia Bueno <natalia.bueno@yale.edu>
2017-01-13 12:58:40 -0200

    first full try

--- filiados_TSE.R
+++ filiados_TSE.R
@@ -22,23 +22,32 @@ parties <- function(ufs_downloaded, parties_downloaded, sobjudice=F){
   br <- locale("es", encoding = "windows-1252") 
   if(sobjudice==F){
   #this avoids encoding errors and it depends on your machine/R setup
-  
   paths_filiados <- as.list(paste0(dfolder, "original_unzipped/", toupper(parties_downloaded),"/", toupper(ufs_downloaded),"/", 
                          "aplic/sead/lista_filiados/uf/filiados_", tolower(parties_downloaded),
                          "_", tolower(ufs_downloaded), ".csv"))
-
+  #checking if files exist, if not remove from list
+  if(length(which(lapply(paths_filiados, file.exists)==FALSE)) > 0) {
+    missing <- which(lapply(paths_filiados, file.exists)==FALSE)
+    paths_filiados <- paths_filiados[-c(as.numeric(as.character(missing)))]
+  }
   party_allstates <- lapply(paths_filiados, read_csv2, locale=br, col_types = cols(.default = col_character())) %>% bind_rows()
   }
-  else{
-  print("Getting filiados sob judice")
+  if(sobjudice==T){
+  print("Note: getting filiados sob judice")
   paths_judice <- as.list(paste0(dfolder, "original_unzipped/", toupper(parties_downloaded),"/", toupper(ufs_downloaded),"/", 
                                  "aplic/sead/lista_filiados/uf/fil_sub_jud_", tolower(parties_downloaded),
                                  "_", tolower(ufs_downloaded), ".csv"))
-  
+  #checking if files exist, if not remove from list
+  if(length(which(lapply(paths_judice, file.exists)==FALSE)) > 0) {
+    missing <- which(lapply(paths_judice, file.exists)==FALSE)
+    paths_judice <- paths_judice[-c(as.numeric(as.character(missing)))]
+  }
   party_allstates <- lapply(paths_judice, read_csv2, locale=br, col_types = cols(.default = col_character())) %>% bind_rows()
   }
   return(party_allstates)
 }
+
+
 
 ####################Downloading
 #Downloading UFs
@@ -85,18 +94,22 @@ filiados_jan2017 <- bind_rows(all, .id = "all")
 all_sobj <- list()
 for (p in 1:length(pparties)){
   all_sobj[[p]] <-    parties(ufs_downloaded=list.files(paste0(dfolder, "original_unzipped/", pparties[p])),
-                         parties_downloaded=pparties[p], sobjudice = T) 
+                              parties_downloaded=pparties[p], sobjudice = T) 
 }
 sobjudice_jan2017 <- bind_rows(all_sobj, .id = "all")
 
 #Basic checks on number of states and number of parties
 #1. Number of states
 length(table(filiados_jan2017$UF))==length(ufs)
+length(table(sobjudice_jan2017$UF))==length(ufs) #missing DF (this is an error)
 #2. Number of parties
-length(table(filiados_jan2017$UF))==length(pparties)
+length(table(filiados_jan2017$`SIGLA DO PARTIDO`))==length(pparties)
+length(table(sobjudice_jan2017$`SIGLA DO PARTIDO`))==length(pparties) #missing two parties #missing NOVO and PCO
 #3. Any state without any party affiliates for any political party? (smell check)
-filiados_jan2017 %>% group_by(`SIGLA DO PARTIDO`, UF) %>% summarize(n_obs = n())
+filiados_jan2017 %>% group_by(`SIGLA DO PARTIDO`, UF) %>% summarize(n_obs = n()) %>% arrange(n_obs)
 #if so, which state(s) and which party (ies)?
 filiados_jan2017 %>% group_by(`SIGLA DO PARTIDO`, UF) %>% summarize(n_obs = n()) %>% filter(n_obs == 0)
+#None
 
-
+save(filiados_jan2017, file=paste0(dfolder, "combined_data/filiados_jan2017.Rda"))
+save(sobjudice_jan2017, file=paste0(dfolder, "combined_data/sobjudice_jan2017_tobecorrected.Rda"))
